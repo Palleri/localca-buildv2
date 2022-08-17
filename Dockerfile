9e8b441fcb3167fc8cdd7a5dc5460f4670415c7f
palleri <palleri@mario.se>
2022-08-17 13:13:44 +0000

    added all the shit

--- Dockerfile
+++ Dockerfile
@@ -1,14 +1,14 @@
-FROM php:8.0-apache
+FROM php:8.1.9-apache
 RUN apt update
 RUN apt install vim openssl sudo -y
-
+#RUN apk add --update openssl sudo vim bash
 
 ENV ca=''
 ENV cakey_FILE=''
 ENV cakey=''
 ENV C=''
 ENV O=''
-COPY run.sh /tmp/run.sh
+COPY run.sh /run.sh
 COPY bin/* /etc/
 COPY src /var/www/
 #COPY index.php /var/www/
@@ -16,4 +16,4 @@ COPY src /var/www/
 #COPY files /var/www/
 #COPY bg.jpg /var/www/
 RUN chown -R www-data:www-data /var/www/* 
-ENTRYPOINT ["/tmp/run.sh", "env"]
+ENTRYPOINT ["/run.sh", "env"]
--- run.sh
+++ run.sh
@@ -11,9 +11,9 @@ if [ ! -z "$cakey_FILE" -a -z "$cakey" ]; then
   cakey=$(cat $cakey_FILE)
 fi
 
-echo $ca
-echo $C
-echo $O
+echo $ca > /var/www/ca.txt
+echo $C > /var/www//C.txt
+echo $O > /var/www/O.txt
 if [ -d "/var/www/html/files/ca" ] 
 then
     echo "Found CA." 
@@ -25,12 +25,15 @@ else
 	echo -n $cakey >> /var/www/html/files/ca/CA_KEY.txt
 	openssl genrsa -des3 -out /var/www/html/files/ca/ca.key -passout pass:$cakey 2048
 	sleep 5s
-	openssl req -x509 -new -nodes -key /var/www/html/files/ca/ca.key -sha256 -days 1825 -out /var/www/html/files/ca/ca.pem -passin pass:$cakey     -subj "/CN=${ca}/C=${C}/O=${O}"
+	openssl req -x509 -new -nodes -key /var/www/html/files/ca/ca.key -sha256 -days 1095 -out /var/www/html/files/ca/ca.pem -passin pass:$cakey     -subj "/CN=${ca}/C=${C}/O=${O}"
 
 
 fi
 
 
+cp /var/www/ca.txt /var/www/html/files/ca/ca.txt
+cp /var/www/C.txt /var/www/html/files/ca/C.txt
+cp /var/www/O.txt /var/www/html/files/ca/O.txt
 cp /var/www/index.php /var/www/html/index.php
 cp /var/www/style.css /var/www/html/style.css
 cp /var/www/bg.jpg /var/www/html/bg.jpg
@@ -40,4 +43,6 @@ cp /var/www/client_cert_san_ext.conf /var/www/html/client_cert_san_ext.conf
 chown -R www-data:www-data /var/www/html
 cp /var/www/html/files/ca/ca.pem /var/www/html/files/ca.pem
 
+#while :; do echo 'Hit CTRL+C'; sleep 1; done
+#/usr/local/sbin/php-fpm --nodaemonize
 apache2-foreground
