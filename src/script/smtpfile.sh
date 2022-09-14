112fddeefd144a7de148e234ee4085d4ed3af52a
Palleri <Skoog.timmy@gmail.com>
2022-09-14 17:35:09 +0200

    edit mail

--- src/script/smtpfile.sh
+++ src/script/smtpfile.sh
@@ -9,4 +9,4 @@
   
   echo "Subject: SSL expire
 One of your SSL certificate will expire in 30 days" >> /var/www/html/script/certwarning.txt
-  echo "SSL warning test mail" >> /var/www/html/script/testmail.txt
+  echo "Subject: SSL warning test" >> /var/www/html/script/testmail.txt
